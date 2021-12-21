549c4b55399e264b2d2000011b670a67724cae5b
luisvfandrade <luis.vilhena@tecnico.ulisboa.pt>
2021-12-21 22:12:56 +0000

    Update

--- proj1/p1.cpp
+++ proj1/p1.cpp
@@ -1,20 +1,70 @@
-/*
-Problema1(X)
-    if |X| == 0
-        return
-    let LIS be an array of size |X|
-    LIS[0] = 1
-    let maxLIS = 1
-    let nLIS = 1
-    for i = 1 to |X| - 1
-        let j = i - 1
-        while X[j] > X[i]
-            j--
-        LIS[i] = LIS[J] + 1
-        if LIS[i] > maxLIS
-            maxLIX = LIS[i]
-            nLIS = 1
-        else if LIS[i] == maxLIS
-            nLIS = nLIS + 1
-    return maxLIS, nLIS
-*/
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+typedef struct sequence {
+    vector<int> values;
+    int size = 0;
+} sequence;
+
+sequence readSequence() {
+    int value, c;
+    sequence x;
+
+    do {
+        //Possible Integer Sequence Error
+        if (scanf("%d", &value) == 0) {
+            fprintf(stderr, "error: Invalid integer sequence.\n");
+            exit(1);
+        }
+        else {
+            x.values.push_back(value);
+            x.size++;
+        }
+    }
+    while ((c = getchar()) != '\n' && c != EOF);
+
+    return x;
+}
+
+vector<int> problem1(sequence x) {
+    vector<int> maxLis{1, x.size};
+    return maxLis;
+}
+
+int problem2(sequence x1, sequence x2) {
+    int maxLcis = 0;
+    return maxLcis;
+}
+
+string resolve() {
+    int problem, c;
+
+    //Possible Problem Number Related Errors
+    if (scanf("%d", &problem) == 0 || (problem != 1 && problem != 2)) {
+        fprintf(stderr, "error: Invalid problem number.\n");
+        exit(1);
+    }
+    else if ((c = getchar()) != '\n') {
+        fprintf(stderr, "error: The first line must contain only the problem number.\n");
+        exit(1);
+    }
+
+    if (problem == 1) {
+        printf("here %d\n", problem);       
+        sequence x = readSequence();
+        vector<int> results = problem1(x);
+        return to_string(results[0]) + " " + to_string(results[1]);
+    }
+    else {
+        sequence x1 = readSequence(), x2 = readSequence();
+        return to_string(problem2(x1, x2));
+    }
+}
+
+int main() {
+    printf("%s\n", resolve().c_str());
+    return 0;
+}
