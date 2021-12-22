8c1c8243c674eed84f74d4948e614ad754f57040
luisvfandrade <luis.vilhena@tecnico.ulisboa.pt>
2021-12-22 16:25:20 +0000

    Update

--- proj1/p1.cpp
+++ proj1/p1.cpp
@@ -1,13 +1,31 @@
+/*
+ * Ficheiro: p1.cpp
+ * Autor: Luis Freire D'Andrade (N94179), Joana Maria de Brito (N96037)
+ * Descricao: Projeto 1 de ASA: desenvolvimento, em linguagem C++, de dois problemas, LIS e LCIS.
+*/
+
+/*
+ * Bibliotecas:
+*/
+
 #include <stdio.h>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+/*
+ * Estruturas:
+*/
+
 typedef struct sequence {
     vector<int> values;
     int size = 0;
 } sequence;
+
+/*
+ * Funcoes:
+*/
 
 sequence readSequence() {
     int value, c;
@@ -30,12 +48,64 @@ sequence readSequence() {
 }
 
 vector<int> problem1(sequence x) {
-    vector<int> maxLis{1, x.size};
+    vector<int> maxLis{0, x.size};
+    if (x.size == 0)
+        return maxLis;
+
+    vector<int> lis(x.size, 1);
+    maxLis[0] = 1;
+    
+    for (int i = 1; i < x.size; i++) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (x.values[j] < x.values[i]) {
+                if (lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                    if (lis[i] > maxLis[0]) {
+                        maxLis[0] = lis[i];
+                        maxLis[1] = 1;
+                    }
+                    else if  (lis[i] == maxLis[0])
+                        maxLis[1]++;
+                }
+                else if (lis[j] + 1 == maxLis[0])
+                    maxLis[1]++;
+            }
+        }
+    }
+
     return maxLis;
 }
 
 int problem2(sequence x1, sequence x2) {
     int maxLcis = 0;
+    if (x1.size == 0 || x2.size == 0)
+        return maxLcis;
+
+    //Optimize spatial complexity.
+    sequence smaller, bigger;
+    if (x1.size <= x2.size) {
+        smaller = x1;
+        bigger = x2;
+    }
+    else {
+        smaller = x2;
+        bigger = x1;
+    }
+
+    vector<int> lcis(smaller.size, 0);
+    for (int i = 0; i < bigger.size; i++) {
+        int currentLcis = 0;
+        for (int j = 0; j < smaller.size; j++) {
+            if (bigger.values[i] == smaller.values[j]) {
+                lcis[j] = max(lcis[j], currentLcis + 1);
+                if (lcis[j] > maxLcis)
+                    maxLcis = lcis[j];
+            }
+            else if (bigger.values[i] > smaller.values[j])
+                currentLcis = max(lcis[j], currentLcis);
+        }
+    }
+
     return maxLcis;
 }
 
@@ -51,9 +121,8 @@ string resolve() {
         fprintf(stderr, "error: The first line must contain only the problem number.\n");
         exit(1);
     }
-
-    if (problem == 1) {
-        printf("here %d\n", problem);       
+    
+    if (problem == 1) {     
         sequence x = readSequence();
         vector<int> results = problem1(x);
         return to_string(results[0]) + " " + to_string(results[1]);
@@ -63,6 +132,10 @@ string resolve() {
         return to_string(problem2(x1, x2));
     }
 }
+
+/*
+ * Funcao Main:
+*/
 
 int main() {
     printf("%s\n", resolve().c_str());
